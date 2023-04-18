8064751dfe106013b9b5fb33072b2785f35110df
JangHyenSeo <wkdgustj02@naver.com>
2023-04-18 17:46:08 +0900

    board pager

---
+++ src/main/java/com/iu/base/board/BoardDAO.java
@@ -0,0 +1,26 @@
+package com.iu.base.board;
+
+import java.util.List;
+
+import com.iu.base.util.Pager;
+
+public interface BoardDAO {
+	
+	// 글갯수
+	public Long getTotalCount(Pager pager) throws Exception;
+	
+	// 글조회
+	public List<BoardVO> getList(Pager pager) throws Exception;
+	
+	// 글 하나 조회
+	public BoardVO getDetail(BoardVO boardVO) throws Exception;
+	
+	// 글쓰기
+	public int setInsert(BoardVO boardVO) throws Exception;
+	
+	// 글수정
+	public int setUpdate(BoardVO boardVO) throws Exception;
+	
+	// 글삭제
+	public int setDelete(BoardVO boardVO) throws Exception;
+}
